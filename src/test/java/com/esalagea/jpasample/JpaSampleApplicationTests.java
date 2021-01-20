36163a8502c5fc199df3125bdb325f61d911eeca
emil <emil.salageanu@gmail.com>
2021-01-20 15:23:12 +0100

    initial commit

---
+++ src/test/java/com/esalagea/jpasample/JpaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esalagea.jpasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
